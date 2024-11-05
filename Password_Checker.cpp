b7f7c7bb7be6afa0fd15859ae98798e3d840e9ee
amiin32 <137880702+amiin32@users.noreply.github.com>
2024-11-06 01:22:52 +0300

    Update Password_Checker.cpp

--- Password_Checker.cpp
+++ Password_Checker.cpp
@@ -1,35 +1,41 @@
-
+	// Main library for input and output( cout & cin )
 	#include <iostream>
-
+	// This is used to mesure time
 	#include <chrono>
-
+	// and this library is for delays
 	#include<thread>
 
 	using namespace std;
 
 	int main()
 	{
+		//variables for password & username
 		string username, password;
-
+		
+		//just console design..
 		cout << "______________________________"<<endl;
 		cout << "-------Password_Checker-------"<<endl;
 		cout << "______________________________"<<endl;
 
 		cout << "Enter your username: ";
 		cin >> username;
+		//This will make delay for 2 seconds to ask password
 		this_thread::sleep_for(chrono::seconds(2));
 		cout << "Enter your password(must be 8-16 digits): ";
 		cin >> password;
+		//This is also delay before the result of the input
 		this_thread::sleep_for(chrono::seconds(5));
-
+		//if the user input met the required
 		if (password.length() >= 8 && password.length() <=16) {
 
 			cout << "Welcome Back sir " << username;
 		}
+		//if the user input is less than expected	
 		else if (password.length() <= 7) {
 
 			cout << "ERROR: Your password is too short!";
 		}
+		//if the user input greater than expected	
 		else if (password.length() >= 17) {
 
 			cout << "ERROR: Your Password is too long to be saved!";
